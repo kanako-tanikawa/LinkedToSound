f53ae9db5369adb8399722b84431cc78dd3d5716
kanako-tanikawa <k1014818k@gmail.com>
2025-02-07 00:42:44 +0900

    オブジェクト選択時にオブジェクトが回転する機能、カメラ、スポットライトの実装

--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 
 public class CubeManager : MonoBehaviour
 {
+    public bool IsCubeSelected = false;
+    [SerializeField] GameManager gameManager; // GameManagerへの参照
 
-    private void Start()
+    private void OnMouseDown() // Cubeがクリックされたとき
     {
-        transform.localScale = new Vector3(5, 5, 5);
+        IsCubeSelected = !IsCubeSelected; // 選択状態を切り替える
     }
 
-    public void SizeChange()
+    private void Update()
     {
-        transform.localScale = new Vector3(10, 10, 10);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsCubeSelected = false;
+        }
+
+        //Cubeの回転
+        if (IsCubeSelected)
+        {
+            transform.Rotate(new Vector3(0, 45 * Time.deltaTime, 0));
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity; // Cubeを元の向きに戻す
+        }
     }
 }
