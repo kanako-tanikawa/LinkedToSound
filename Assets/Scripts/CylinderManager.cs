8869f3a04cff5b3175d45d5a3b15c115d3c6bdaa
kanako-tanikawa <k1014818k@gmail.com>
2025-02-15 17:52:15 +0900

    MIDIとの連動を実現、Playシーンの作成

--- Assets/Scripts/CylinderManager.cs
+++ Assets/Scripts/CylinderManager.cs
@@ -11,4 +11,14 @@ public class SylinderManager : SelectableObject
     {
         base.ResetObject();
     }
+
+    public override void LargeSize(float speed)
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), speed);
+    }
+
+    public override void SmallSize(float speed)
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.position, new Vector3(-5, 0, 0), speed);
+    }
 }
--- Assets/Scripts/SphereManager.cs
+++ Assets/Scripts/SphereManager.cs
@@ -11,4 +11,14 @@ public class SphereManager : SelectableObject
     {
         base.ResetObject();
     }
+
+    public override void LargeSize(float speed)
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public override void SmallSize(float speed)
+    {
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
 }
